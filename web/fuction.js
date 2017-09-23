b4588a6202a4a3ffb4d5f973484bea26a6cfbcd1
educacionit <educacionit@alumno-1003-16>
2017-09-23 12:00:46 -0300

    javascript_.js

--- web/fuction.js
+++ web/fuction.js
@@ -1,4 +1,10 @@
-function writeInput(txt){
-          span = document.getElementById("texto");
-          span.innerHTML = txt;
-	}
+function writeInput(txt) {
+    span = document.getElementById("texto");
+    span.innerHTML = txt;
+}
+
+function show(Persona) {
+    nombre = Persona.nombre;
+    apellido = Persona.apellido;
+    alert(nombre, apellido);
+}
